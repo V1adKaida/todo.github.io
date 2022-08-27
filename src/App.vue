c1ef7a49587abc8cb4019e9459067e12c3cfd38e
Vlad Kaida <vladkaiday@gmail.com>
2022-08-27 14:21:23 +0300

    build

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,6 @@
   <notifications position="bottom right" />
 </template>
 
-<script>
-
-export default {
-
-  mounted() {
-    this.$store.dispatch('state')
-  }
-}
-</script>
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap');
 
